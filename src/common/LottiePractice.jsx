7df6581d5c19d8f8e85af63fc9d3903d19953708
Seema-476 <simmybishnoi29085@gmail.com>
2024-11-04 09:35:34 +0530

    lootie animation done

--- src/common/LottiePractice.jsx
+++ src/common/LottiePractice.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-// import Lottie from 'react-lottie';
-// import Watch from '../assets/lottie/watch.json'
+import Lottie from 'lottie-react'
+import Cartoon from '../assets/lottie/cartoon.json'
 
 const LottiePractice = () => {
     return (
-        <div>
-            <h3 className='font-inter font-semibold text-4xl text-black text-center pb-9 animate__animated animate__flipInX animate__slow 1s'>LOTTIEPRACTICE </h3>
-            <div>
-
-                {/* <Lottie
-                    Watch={Watch}
-                    options={defaultOptions}
-                    height={400}
-                    width={400}
-                    isStopped={this.state.isStopped}
-                    isPaused={this.state.isPaused} /> */}
+        <div className='py-12'>
+            <h3 className='font-inter font-semibold text-4xl text-black text-center pb-9 animate__animated animate__flipInX animate__slow 1s'>LOTTIE PRACTICE </h3>
+            <div className='max-w-[300px] text-center mx-auto'>
+                <Lottie animationData={Cartoon} loop={true} speed={2} />
             </div>
         </div>
 
