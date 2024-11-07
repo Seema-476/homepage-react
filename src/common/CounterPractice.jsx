431563dd856ad3117dc6e1727cc9075a4b56c0cf
Seema-476 <simmybishnoi29085@gmail.com>
2024-11-07 11:37:23 +0530

    changes

--- src/common/CounterPractice.jsx
+++ src/common/CounterPractice.jsx
@@ -3,7 +3,7 @@ import CountUp from 'react-countup'
 import ReactVisibilitySensor from 'react-visibility-sensor'
 
 const CounterPractice = () => {
-    const [ isVisible,setIsVisible] = useState(false)
+    const [isVisible, setIsVisible] = useState(false)
     function onVisible(isVisible) {
         if (isVisible) {
             setIsVisible(true)
@@ -16,11 +16,11 @@ const CounterPractice = () => {
                 <div className='text-center pt-5'>
                     <ReactVisibilitySensor onChange={onVisible}>
                         <div>
-                            <CountUp start={0} end={50} delay={1}>
+                            <CountUp start={0} end={isVisible ? 100 : 0} delay={1}>
                             </CountUp>
-                       </div>
-                  </ReactVisibilitySensor>
-               </div>
+                        </div>
+                    </ReactVisibilitySensor>
+                </div>
             </div>
         </div>
     )
