48324163b6adac9a10f0c0bb9bb3336172c39a5c
Seema-476 <simmybishnoi29085@gmail.com>
2024-11-11 09:25:38 +0530

    keyframe animation done

--- src/common/AnimationKeyFrame.jsx
+++ src/common/AnimationKeyFrame.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 
 const AnimationKeyFrame = () => {
   return (
-    <div className='p y-12'>
-
+    <div className='py-12'>
+      <div className='container'>
+        <h3 className='font-inter font-semibold text-4xl text-black text-center pb-10'>KeyFrame Animation </h3>
+        <div className='flex items-center justify-center'>
+          <div className='keyframe'></div>
+        </div>
+      </div>
     </div>
   )
 }
