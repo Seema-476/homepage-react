562e9c502cc80faeed8b20c2415ae49cf364bf99
Seema-476 <simmybishnoi29085@gmail.com>
2024-11-15 21:45:26 +0530

    changes

--- src/common/CustomTabs.jsx
+++ src/common/CustomTabs.jsx
@@ -14,27 +14,27 @@ const CustomTabs = () => {
                   <button
                       id="tab-1"
                       onClick={handleClick}
-                      className={`py-4 px-5 rounded font-medium text-2xl text-black ${active === 'tab-1' ? 'active':'!text-yellow'}`}>
+                      className={`py-3 px-5 rounded font-medium text-2xl text-black ${active === 'tab-1' ? 'text-white bg-black' :'text-black'}`}>
                       Btn 1
                   </button>
                   <button
                       id="tab-2"
                       onClick={handleClick}
-                      className={`py-4 px-5 rounded font-medium text-2xl text-black ${ active === 'tab-1' ? 'active' : '!text-yellow'}`}>
+                      className={`py-3 px-5 rounded font-medium text-2xl text-black ${active === 'tab-2' ? 'text-white bg-black' : 'text-black'}`}>
                       Btn 2
                   </button>
                   <button
                       id="tab-3"
                       onClick={handleClick}
-                      className={`py-4 px-5 rounded font-medium text-2xl text-black ${ active === 'tab-1' ? 'active' : '!text-yellow'}`}>
+                      className={`py-3 px-5 rounded font-medium text-2xl text-black ${active === 'tab-3' ? 'text-white bg-black' : 'text-black'}`}>
                       Btn 3
                   </button>
               </div>
               </div>
               <div>
-                  {active === 'tab-1' &&( <p className='text-normal text-base max-w-[400px] p-3 bg-slate-400 text-white rounded mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur non expedita aperiam totam esse aut pariatur facilis accusamus odio, vel sit, qui voluptatibus.</p>)}
-                  {active === 'tab-2' &&(<p className='text-normal text-base max-w-[400px] p-3 bg-gray-900 text-white rounded mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur non expedita aperiam totam esse aut pariatur facilis accusamus odio, vel sit, qui voluptatibus.</p>)}
-                  {active === 'tab-3' &&(<p className='text-normal text-base max-w-[400px] p-3 bg-slate-700 text-white rounded mx-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur non expedita aperiam totam esse aut pariatur facilis accusamus odio, vel sit, qui voluptatibus.</p>)}
+                  {active === 'tab-1' &&( <p className='text-normal text-base max-w-[400px] p-3 bg-slate-500 text-white rounded mx-auto'><span className='font-bold text-yellow-400'>1.</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur non expedita aperiam totam esse aut pariatur facilis accusamus odio, vel sit, qui voluptatibus.</p>)}
+              {active === 'tab-2' && (<p className='text-normal text-base max-w-[400px] p-3 bg-gray-900 text-white rounded mx-auto'><span className='font-bold text-yellow-400'>2.</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur non expedita aperiam totam esse aut pariatur facilis accusamus odio, vel sit, qui voluptatibus.</p>)}
+              {active === 'tab-3' && (<p className='text-normal text-base max-w-[400px] p-3 bg-slate-700 text-white rounded mx-auto'><span className='font-bold text-yellow-400'>3.</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit tenetur non expedita aperiam totam esse aut pariatur facilis accusamus odio, vel sit, qui voluptatibus.</p>)}
               </div>
           </div>
   )
